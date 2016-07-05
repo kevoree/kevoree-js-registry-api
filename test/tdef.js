3fe148c5ba633b087645208ac977272afad50f10
Maxime Tricoire <max.tricoire@gmail.com>
2016-07-05 18:10:49 +0200

    tdef is ok

--- test/tdef.js
+++ test/tdef.js
@@ -38,6 +38,20 @@ describe('Tdef test', function () {
       .catch(done);
   });
 
+  it('should return 404 on unknown tdef Yolo/8.5.9 in "foo"', function (done) {
+    api.tdef({
+        namespace: 'foo',
+        name: 'Yolo',
+        version: '8.5.9'
+      })
+      .get()
+      .catch(function (err) {
+        expect(err).toExist();
+        expect(err.code).toEqual(404);
+        done();
+      });
+  });
+
   it('should create a new FakeType/1.2.3 in the "kevoree" namespace', function () {
     return api.tdef({
         namespace: 'kevoree',
@@ -46,6 +60,54 @@ describe('Tdef test', function () {
         model: '{}'
       })
       .create();
+  });
+
+  it('should return 403 when not member of namespace', function (done) {
+    api.tdef({
+        namespace: 'user',
+        name: 'WontHappen',
+        version: '1.2.3',
+        model: '{}'
+      })
+      .create()
+      .catch(function (err) {
+        expect(err).toExist();
+        expect(err.code).toEqual(403);
+        done();
+      });
+  });
+
+  it('should return 401 when not authenticated', function (done) {
+    // forget user:token => not connected
+    nconf.set('user:token', 'wontwork');
+
+    api.tdef({
+        namespace: 'user',
+        name: 'WontHappen',
+        version: '1.2.3',
+        model: '{}'
+      })
+      .create()
+      .catch(function (err) {
+        expect(err).toExist();
+        expect(err.code).toEqual(401);
+        done();
+      });
+  });
+
+  it('should return 404 when namespace does not exist', function (done) {
+    api.tdef({
+        namespace: 'yolo',
+        name: 'WontHappen',
+        version: '1.2.3',
+        model: '{}'
+      })
+      .create()
+      .catch(function (err) {
+        expect(err).toExist();
+        expect(err.code).toEqual(404);
+        done();
+      });
   });
 
   it('should delete FakeType/1.2.3 in the "kevoree" namespace', function () {
